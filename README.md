ff4c9247cce8f6d2864bfb01d0094a00358209a4
kaunild <dhruv.kaunil@gmail.com>
2020-04-04 08:57:31 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# image_mosaicing
+ Aligns a pair of images using SuperPoint Feature extractor
---
+++ src/imagemosaicing.cpp
@@ -0,0 +1,12 @@
+﻿// Image Mosaicing.cpp : Defines the entry point for the application.
+//
+
+#include "Image Mosaicing.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
